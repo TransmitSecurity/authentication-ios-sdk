4a0ff81cd8f440e8aa44a5e44dbf1a5b751d81cf
Omar Ayed <omar@transmitsecurity.com>
2023-01-25 06:41:01 +0200

    MOB-275 #add publish files

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+
+import PackageDescription
+
+let package = Package(
+    name: "TSAuthenticationSDK",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "TSAuthenticationSDK",
+            targets: ["TSAuthenticationSDK"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/TransmitSecurity/core-ios-sdk.git", from: "1.0.6")
+    ],
+    targets: [
+        .binaryTarget(
+            name: "TSAuthenticationSDK",
+            path: "Sources/TSAuthenticationSDK.xcframework"
+        ),
+        .target(name: "TSAuthenticationSDK-Dependencies",
+                dependencies: [
+                    .product(name: "TSCoreSDK",
+                             package: "core-ios-sdk")
+                ]),
+    ]
+)
