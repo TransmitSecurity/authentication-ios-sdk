bb3c878ae3b462e7639071a8f8c7d0318a634093
Tomer Picker <>
2025-05-29 17:14:00 +0300

    release 1.1.15

--- build/TSAuthenticationSDK.xcframework/Docs/Build/Intermediates.noindex/TSAuthenticationSDK.build/Debug-iphoneos/TSAuthenticationSDK.build/Objects-normal/arm64/TSAuthenticationSDK_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ec2-user/actions-runner/_work/mobile-ios-authentication-sdk/mobile-ios-authentication-sdk/mobile-ios-authentication-sdk/docs/Build/Intermediates.noindex/TSAuthenticationSDK.build/Debug-iphoneos/TSAuthenticationSDK.build/module.modulemap \
-  /Users/ec2-user/actions-runner/_work/mobile-ios-authentication-sdk/mobile-ios-authentication-sdk/mobile-ios-authentication-sdk/docs/Build/Intermediates.noindex/TSAuthenticationSDK.build/Debug-iphoneos/TSAuthenticationSDK.build/DerivedSources/TSAuthenticationSDK_vers.c
