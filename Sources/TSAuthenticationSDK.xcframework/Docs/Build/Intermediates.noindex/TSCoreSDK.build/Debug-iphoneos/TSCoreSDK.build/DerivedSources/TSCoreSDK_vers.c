f394a930088202480ef1b88b7d34d3040ea4e7ba
Igor Babitski <igor.babitski@transmitsecurity.com>
2025-05-31 23:36:55 +0300

    Removed unnecessary files

--- Sources/TSAuthenticationSDK.xcframework/Docs/Build/Intermediates.noindex/TSCoreSDK.build/Debug-iphoneos/TSCoreSDK.build/DerivedSources/TSCoreSDK_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char TSCoreSDKVersionString[];
- extern const double TSCoreSDKVersionNumber;
-
- const unsigned char TSCoreSDKVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:TSCoreSDK  PROJECT:TSCoreSDK-1.0.31" "\n";
- const double TSCoreSDKVersionNumber __attribute__ ((used)) = (double)1.0;
--- Sources/TSAuthenticationSDK.xcframework/Docs/Build/Intermediates.noindex/TSCoreSDK.build/Debug-iphoneos/TSCoreSDK.build/Objects-normal/arm64/TSCoreSDK_vers.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/ec2-user/actions-runner/_work/mobile-ios-authentication-sdk/mobile-ios-authentication-sdk/mobile-ios-authentication-sdk/docs/Build/Intermediates.noindex/TSCoreSDK.build/Debug-iphoneos/TSCoreSDK.build/module.modulemap \
-  /Users/ec2-user/actions-runner/_work/mobile-ios-authentication-sdk/mobile-ios-authentication-sdk/mobile-ios-authentication-sdk/docs/Build/Intermediates.noindex/TSCoreSDK.build/Debug-iphoneos/TSCoreSDK.build/DerivedSources/TSCoreSDK_vers.c
